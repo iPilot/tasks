3e4d73f6c76500c510412c766d24e116bd08264c
RomanKosukhin <www.namor.ru@mail.ru>
2018-06-24 12:21:41 +0400

    eval2 fix

--- Eval/EvalProgram.cs
+++ Eval/EvalProgram.cs
@@ -7,7 +7,13 @@ namespace EvalTask
 	{
 		static void Main(string[] args)
 		{
-			string input = Console.In.ReadToEnd();
+			string input = Console.ReadLine();
+			string json = Console.In.ReadToEnd();
+
+			if (!string.IsNullOrWhiteSpace(json))
+			{
+				input = new StringConverter().ConvertString(input, json);
+			}
 			var parsedString = input.Replace(',', '.');
 			string output = new Evaluator().Evaluate(parsedString).ToString();
 			Console.WriteLine(output, CultureInfo.InvariantCulture);
