45e393841b2f1a22ffff4afbc1d707fb73ffe51e
RomanKosukhin <www.namor.ru@mail.ru>
2018-06-24 12:18:13 +0400

    Eval2 : string converter

--- Eval/Evaluator_Should.cs
+++ Eval/Evaluator_Should.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using System.IO;
 
 namespace EvalTask
 {
@@ -14,7 +15,6 @@ namespace EvalTask
 
 		[SetUp]
 		public void SetUp() => Evaluator = new Evaluator();
-
 
 		[TestCase("2+2", ExpectedResult = 4)]
 		[TestCase("2+2*2", ExpectedResult = 6)]
@@ -29,6 +29,28 @@ namespace EvalTask
 		public double AnswerIs4_WhenSomething(string input)
 		{
 			return Evaluator.Evaluate(input);
+		}
+	}
+
+	[TestFixture]
+	public class stringConverter_Should
+	{
+		public IStringConverter StringConverter { get; private set; }
+		public string simpleJson = File.ReadAllText(@"C: \Users\User3\Desktop\Новая папка\sapmple.txt");
+
+		[SetUp]
+		public void SetUp()
+		{
+			StringConverter = new StringConverter();
+		}
+
+
+		[TestCase("a+b", ExpectedResult = "1+2")]
+		[TestCase("a+a+b", ExpectedResult = "1+1+2")]
+		[TestCase("a+a+b+2", ExpectedResult = "1+1+2+2")]
+		public string DoSomething_WhenSomething(string input)
+		{
+			return StringConverter.ConvertString(input, simpleJson);
 		}
 	}
 }
---
+++ Eval/IStringConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace EvalTask
+{
+	public interface IStringConverter
+	{
+		string ConvertString(string expression, string json);
+	}
+
+	public class StringConverter : IStringConverter
+	{
+		public string ConvertString(string expression, string json)
+		{
+			var jObject = JObject.Parse(json);
+
+			foreach (var item in jObject)
+			{
+				expression = expression.Replace(item.Key, item.Value.ToString());
+			}
+
+			return expression;
+		}
+	}
+}
