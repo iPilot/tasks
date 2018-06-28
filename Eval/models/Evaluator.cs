bcdc772967080fd5c76d3438a91989701bc1947b
iPilot <ipilot93@gmail.com>
2018-06-28 15:42:36 +0400

    Evaluator.cs clean up

--- Eval/models/Evaluator.cs
+++ Eval/models/Evaluator.cs
@@ -82,16 +82,4 @@ namespace EvalTask
 			return Evaluate(expression, new Dictionary<string, double>());
 		}
 	}
-
-				public static class DynamicExpression 
-				{
-					public static double Eval() 
-					{
-						
-						return 0;
-					}
-					public static double min(double a, double b) => Math.Min(a, b);
-					public static double max(double a, double b) => Math.Max(a, b);
-					public static double sqrt(double a) => Math.Sqrt(a);
-				}
 }
